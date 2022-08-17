84da1367a57cc94254b4840457df51d381e6045e
Akrem Jaber <akrem.jaber@esprit.tn>
2022-08-17 10:39:30 +0100

    DropDownList

--- src/app/Shared/Services/DropDown/drop-down.service.ts
+++ src/app/Shared/Services/DropDown/drop-down.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 import { Person } from '../../Models/Person/person.model';
 import { Tenant } from '../../Models/Tenant/tenant.model';
 import * as _ from 'lodash';
-import { map } from 'rxjs';
+import { map, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -16,20 +16,20 @@ export class DropDownService {
   list: Person[];
   listTenants:Tenant[];
 
-  // getPersons():any{
-  //   //return this.http.get(this.baseURL)
+  getPersons():any{
+    return this.http.get(this.baseURL)}
     
   //   const res =this.http.get(this.baseURL)
   //   .toPromise()
   //   .then(res => {this.list = res as Person[]});
   // }
-  getPersons(){
-    return this.http.get<[]>(this.baseURL)
-    .pipe(map(data => {
-     this.list=data;
-     return; 
-    }));
-  }
+  // getPersons():any{
+  //   return this.http.get<[]>(this.baseURL)
+  //   .pipe(map(data => {
+  //    this.list=data;
+  //    return; 
+  //   }));
+  // }
 
   // getTenants():any{
   //   //return this.http.get(this.baseURL)
@@ -38,20 +38,28 @@ export class DropDownService {
   //   .toPromise()
   //   .then(res => {this.listTenants = res as Tenant[]});
   // }
-  getTenants(){
-    return this.http.get<[]>(this.tenants)
-    .pipe(map(data=>{
-      this.listTenants=data;
-      return;
-    }));
+  getTenants():any{
+    return this.http.get(this.tenants)
+    // .pipe(map(data=>{
+    //   this.listTenants=data;
+    //   return;
+    // }));
   }
 
-  getDropDownText(id:any, object:any){
+  getDropDownText(uiD_Person:any, object:any):any{
     const selObj = _.filter(object, function (o) {
-        return ( _.includes(id,o.id));
+        return ( _.includes(uiD_Person,o.uiD_Person));
         
     });
     return selObj;
+    
+  }
+  getDropDownTextTenant(uiD_CCCTenants:any, object:any):any{
+    const selObjT = _.filter(object, function (o) {
+        return ( _.includes(uiD_CCCTenants,o.uiD_CCCTenants));
+        
+    });
+    return selObjT;
     
   }
 
--- src/app/dp-multi/dp-multi.component.ts
+++ src/app/dp-multi/dp-multi.component.ts
@@ -10,11 +10,20 @@ export class DpMultiComponent implements OnInit {
 
   constructor(public service:DropDownService) { }
   public thp:any;
+  public t:any;
 
-  get():any{
+  get(){
     this.service.getPersons().subscribe((res: any)=>
       {
         this.thp=res
+        console.log(res)
+      }
+      );
+   } 
+   getT(){
+    this.service.getTenants().subscribe((res: any)=>
+      {
+        this.t=res
         console.log(res)
       }
       );
@@ -23,13 +32,20 @@ export class DpMultiComponent implements OnInit {
    mySelect = [];
    selectedValue: any;
    selectChange() {
-       this.selectedValue = this.service.getDropDownText(this.mySelect, this.thp);
+       this.selectedValue = this.service.getDropDownText(this.mySelect,this.thp );
+   }
+   mySelectTenant = [];
+   selectedValueTenant: any;
+   selectChangeTenant() {
+       this.selectedValueTenant = this.service.getDropDownTextTenant(this.mySelectTenant,this.t );
    }
 
 
   ngOnInit(): void {
     this.service.getPersons().subscribe();
     this.service.getTenants().subscribe();
+    this.get();
+    this.getT();
   }
 
 }
